d0fd47599dadb9e87ad41318c84c9673bb6dc468
Orhan Balci <orhanbalci@gmail.com>
2022-10-24 23:28:28 +0300

    add svg path example to documentation

--- roughr/src/lib.rs
+++ roughr/src/lib.rs
@@ -115,6 +115,32 @@
 //! ### 🖨️ Output Ellipse
 //! ![ellipse](https://raw.githubusercontent.com/orhanbalci/rough-rs/main/roughr/assets/ellipse.png)
 //!
+//!
+//! ### Svg Path
+//!
+//! ```ignore
+//! let options = OptionsBuilder::default()
+//!     .stroke(Srgb::from_raw(&[114u8, 87u8, 82u8]).into_format())
+//!     .fill(Srgb::from_raw(&[254u8, 246u8, 201u8]).into_format())
+//!     .fill_style(FillStyle::Hachure)
+//!     .fill_weight(DPI * 0.01)
+//!     .build()
+//!     .unwrap();
+//! let generator = KurboGenerator::new(options);
+//! let heart_svg_path  = "M140 20C73 20 20 74 20 140c0 135 136 170 228 303 88-132 229-173 229-303 0-66-54-120-120-120-48 0-90 28-109 69-19-41-60-69-108-69z".into();
+//! let heart_svg_path_drawing = generator.path::<f32>(heart_svg_path);
+//! let background_color = Color::from_hex_str("96C0B7").unwrap();
+//!
+//! rc.fill(
+//!     Rect::new(0.0, 0.0, WIDTH as f64, HEIGHT as f64),
+//!     &background_color,
+//! );
+//! heart_svg_path_drawing.draw(&mut rc);
+//! ```
+//!
+//! ### 🖨️ Output Svg Path
+//! ![svgheart](https://raw.githubusercontent.com/orhanbalci/rough-rs/main/roughr/assets/heart_svg_path.png)
+//!
 //! ## 🔭 Examples
 //!
 //! For more examples have a look at the
