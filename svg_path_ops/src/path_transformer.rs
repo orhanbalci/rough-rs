8c3d49cd04a3c701f93c47e7011b40c77407ccc9
orhanbalci <orhanbalci@gmail.com>
2024-09-16 17:02:32 +0300

    add applying svg transform attribute string

--- svg_path_ops/src/path_transformer.rs
+++ svg_path_ops/src/path_transformer.rs
@@ -1,9 +1,9 @@
 use cgmath::{Angle, Deg, Matrix3, Vector2, Vector3};
-use svgtypes::{PathParser, PathSegment};
+use svgtypes::{PathParser, PathSegment, TransformListParser, TransformListToken};
 
 pub struct PathTransformer {
     path_segments: Vec<PathSegment>,
-    stack: Vec<Matrix3<f32>>,
+    stack: Vec<Matrix3<f64>>,
 }
 
 impl PathTransformer {
@@ -22,18 +22,18 @@ impl PathTransformer {
         }
     }
 
-    pub fn translate(&mut self, tx: f32, ty: f32) -> &mut Self {
+    pub fn translate(&mut self, tx: f64, ty: f64) -> &mut Self {
         self.stack
             .push(Matrix3::from_translation(Vector2::new(tx, ty)));
         self
     }
 
-    pub fn scale(&mut self, sx: f32, sy: f32) -> &mut Self {
+    pub fn scale(&mut self, sx: f64, sy: f64) -> &mut Self {
         self.stack.push(Matrix3::from_nonuniform_scale(sx, sy));
         self
     }
 
-    pub fn rotate(&mut self, angle: f32, rx: f32, ry: f32) -> &mut Self {
+    pub fn rotate(&mut self, angle: f64, rx: f64, ry: f64) -> &mut Self {
         self.stack.push(Matrix3::from_axis_angle(
             Vector3::new(rx, ry, 0.0),
             Deg(angle),
@@ -41,23 +41,51 @@ impl PathTransformer {
         self
     }
 
-    pub fn skew_x(&mut self, degrees: f32) -> &mut Self {
+    pub fn skew_x(&mut self, degrees: f64) -> &mut Self {
         let skew_xmatrix = Matrix3::new(1.0, 0.0, 0.0, Deg(degrees).tan(), 1.0, 0.0, 0.0, 0.0, 1.0);
         self.stack.push(skew_xmatrix);
         self
     }
 
-    pub fn skew_y(&mut self, degrees: f32) -> &mut Self {
+    pub fn skew_y(&mut self, degrees: f64) -> &mut Self {
         let skew_ymatrix = Matrix3::new(1.0, Deg(degrees).tan(), 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0);
         self.stack.push(skew_ymatrix);
         self
     }
 
-    pub fn matrix(&mut self, matrix: [f32; 6]) -> &mut Self {
+    pub fn matrix(&mut self, matrix: [f64; 6]) -> &mut Self {
         let converted = Matrix3::new(
             matrix[0], matrix[3], 0.0, matrix[1], matrix[4], 0.0, matrix[2], matrix[5], 1.0,
         );
         self.stack.push(converted);
+        self
+    }
+
+    pub fn transform(&mut self, transform: String) -> &mut Self {
+        let parser = TransformListParser::from(transform.as_str());
+        for path_transform in parser {
+            match path_transform {
+                Ok(pt) => {
+                    self.apply_token(pt);
+                }
+                Err(_) => {
+                    println!("Can not parse transform string.");
+                }
+            }
+        }
+        self
+    }
+
+    fn apply_token(&mut self, token: TransformListToken) -> &mut Self {
+        match token {
+            TransformListToken::Matrix { a, b, c, d, e, f } => self.matrix([a, b, c, d, e, f]),
+            TransformListToken::Translate { tx, ty } => self.translate(tx, ty),
+            TransformListToken::Scale { sx, sy } => self.scale(sx, sy),
+            TransformListToken::Rotate { angle } => self.rotate(angle, 0.0, 0.0),
+            TransformListToken::SkewX { angle } => self.skew_x(angle),
+            TransformListToken::SkewY { angle } => self.skew_y(angle),
+        };
+
         self
     }
 }
